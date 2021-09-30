372adc6e608f41a6027be77124bf399a0b9310ba
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-09-30 20:10:49 +0530

    :card_index: Adding hello component in pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from 'next'
 import styles from 'styles/Home.module.scss'
+import Hello from 'src/components/Hello'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <h1>Hello Next.js</h1>
+      <Hello />
     </div>
   )
 }
